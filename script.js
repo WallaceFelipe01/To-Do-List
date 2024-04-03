7db99f97f1b28bf056b03c52f57b99095a9a857e
Wallace Felipe <wallace.felipe@hotmail.com>
2024-04-02 23:15:54 -0300

    Commit inicial - Código Inicial

---
+++ script.js
@@ -0,0 +1,60 @@
+const localStorageKey = 'to-do-list-gn'
+
+function validateIfExistsNewTask()
+{
+    let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]")
+    let inputValue = document.getElementById('input-new-task').value
+    let exists = values.find(x => x.name == inputValue)
+    return !exists ? false :  true
+}
+
+function newTask()
+{
+    let input = document.getElementById('input-new-task')
+    input.style.border = ''
+
+    //Validacao
+    if(!input.value)
+    {
+        alert('Digite algo para inserir em sua lista')
+    }
+    else if(validateIfExistsNewTask())
+    {
+        input.style.border = '1px solid red'
+        alert('Já existe uma task com essa descrição')
+    }
+    else
+    {
+        let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]")
+        values.push({
+            name: input.value
+        })
+        localStorage.setItem(localStorageKey,JSON.stringify(values))
+        showValues()
+    }
+    input.value = ''
+}
+
+function showValues()
+{
+    let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]")
+    let list = document.getElementById('to-do-list')
+    list.innerHTML = ''
+    for(let i = 0; i < values.length; i++)
+    {
+        list.innerHTML += `<li>${values[i]['name']}<button id='btn-remove' onclick='removeItem("${values[i]['name']}")'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+        <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+      </svg></button></li>`
+    }
+}
+
+function removeItem(data)
+{
+    let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]")
+    let index = values.findIndex(x => x.name == data)
+    values.splice(index,1)
+    localStorage.setItem(localStorageKey,JSON.stringify(values))
+    showValues()
+}
+showValues()
